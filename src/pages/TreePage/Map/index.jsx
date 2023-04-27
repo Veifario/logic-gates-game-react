aa8241bbf54f468f03f7e611fee4db92261c30af
Veifario <veifario.git@gmail.com>
2023-04-27 16:26:34 +0500

    changes

--- src/pages/TreePage/Map/index.jsx
+++ src/pages/TreePage/Map/index.jsx
@@ -5,26 +5,19 @@ import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 const Map = () => {
-  const lvls = useSelector((state) => state.progress.lvlsAccess);
+	const lvls = useSelector((state) => state.progress.lvlsAccess);
 
-  const displayLvls = () =>
-    lvls.map(({ active, className }, index, props) => (
-      <Lvl
-        key={index}
-        id={index + 1}
-        active={active}
-        className={className}
-      />
-    ));
-	
+	const displayLvls = () =>
+		lvls.map(({ active, className }, index, props) => (
+			<Lvl key={index} id={index + 1} active={active} className={className} />
+		));
 
-  return (
-    <div className={s.root}>
-      <div className={s.container}>{displayLvls()}</div>
-
-      <ToastContainer />
-    </div>
-  );
+	return (
+		<div className={s.root}>
+			<div className={s.container}>{displayLvls()}</div>
+			<ToastContainer />
+		</div>
+	);
 };
 
 export default Map;
